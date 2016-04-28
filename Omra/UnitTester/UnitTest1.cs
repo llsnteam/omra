d93b63e457001f5edc670602178a771231d1e816
llsnteam <llsnteam@gmail.com>
2016-04-28 20:04:26 +0200

    teszting

--- Omra/UnitTester/UnitTest1.cs
+++ Omra/UnitTester/UnitTest1.cs
@@ -9,35 +9,6 @@ namespace UnitTester
     [TestClass]
     public class UnitTest1
     {
-        [TestClass]
-        public class UnitTester
-        {
-            [TestMethod]
-            public void TestBizonyitek()
-            {
-                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
-                object returnType = k.Bizonyítékkeresés("").First();
-
-                Assert.AreEqual(new Bizonyitekok(), returnType, "Jó");
-            }
-
-            [TestMethod]
-            public void TestBuneset()
-            {
-                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
-                object returnType = k.Bűnesetkeresés("").First();
-
-                Assert.AreEqual(typeof(Bűneset), returnType, "Jó");
-            }
-
-            [TestMethod]
-            public void TestGyanusitott()
-            {
-                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
-                object returnType = k.Gyanúsítottkeresés("").First();
-
-                Assert.AreEqual(typeof(Gyanúsított), returnType, "Jó");
-            }
-        }
+       
     }
 }
