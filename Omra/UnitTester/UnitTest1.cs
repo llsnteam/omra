b444fdf07a05bc1c69f414be2f35bcc107c47d52
llsnteam <llsnteam@gmail.com>
2016-04-28 19:52:15 +0200

    tesztelés

---
+++ Omra/UnitTester/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Omra;
+using Adatkezelő;
+using System.Linq;
+
+namespace UnitTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class UnitTester
+        {
+            [TestMethod]
+            public void TestBizonyitek()
+            {
+                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
+                object returnType = k.Bizonyítékkeresés("").First();
+
+                Assert.AreEqual(new Bizonyitekok(), returnType, "Jó");
+            }
+
+            [TestMethod]
+            public void TestBuneset()
+            {
+                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
+                object returnType = k.Bűnesetkeresés("").First();
+
+                Assert.AreEqual(typeof(Bűneset), returnType, "Jó");
+            }
+
+            [TestMethod]
+            public void TestGyanusitott()
+            {
+                Adatkezelő.Kereséskezelő k = new Adatkezelő.Kereséskezelő();
+                object returnType = k.Gyanúsítottkeresés("").First();
+
+                Assert.AreEqual(typeof(Gyanúsított), returnType, "Jó");
+            }
+        }
+    }
+}
